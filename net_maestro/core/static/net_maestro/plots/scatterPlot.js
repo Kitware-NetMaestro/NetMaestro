9a65c35643eac1f8b45b128da5cf28f49c1780e7
Brian Helba <brian.helba@kitware.com>
2026-02-17 10:49:33 -0500

    Biome automated fixes

--- net_maestro/core/static/net_maestro/plots/scatterPlot.js
+++ net_maestro/core/static/net_maestro/plots/scatterPlot.js
@@ -3,7 +3,7 @@
  * Displays ROSS simulation data as a scatter plot with configurable axes.
  */
 document.addEventListener('alpine:init', () => {
-    Alpine.data('scatterPlot', () => ({
+  Alpine.data('scatterPlot', () => ({
     // Component state
     records: [],
     columns: [],
@@ -14,120 +14,126 @@ document.addEventListener('alpine:init', () => {
     isLoaded: false,
 
     get valueList() {
-        const excluded_columns = ["PE_ID", "real_time", "virtual_time"]
-        const filtered_columns = this.columns.filter(column => column && !excluded_columns.includes(column))
-        return filtered_columns.map((value) => ({
-            key: value,
-            label: value.replaceAll("_", " ")
-        }))
+      const excludedColumns = ['PE_ID', 'real_time', 'virtual_time'];
+      const filteredColumns = this.columns.filter(
+        (column) => column && !excludedColumns.includes(column),
+      );
+      return filteredColumns.map((value) => ({
+        key: value,
+        label: value.replaceAll('_', ' '),
+      }));
     },
 
     /**
      * Initialize the component and set up watchers.
      */
     init() {
-        this.$watch('$store.dataStore.loadTick', () => {
-            this.load();
-        });
+      this.$watch('$store.dataStore.loadTick', () => {
+        this.load();
+      });
     },
 
     /**
      * Initialize the Plotly scatter plot.
      */
     initPlot() {
-        if (this.isPlotInitialized) {
-            return;
-        }
-        this.isPlotInitialized = true;
-        this.scatterPlotEl = document.getElementById('scatterPlot');
+      if (this.isPlotInitialized) {
+        return;
+      }
+      this.isPlotInitialized = true;
+      this.scatterPlotEl = document.getElementById('scatterPlot');
 
-        const data = [{
-            x: [],
-            y: [],
-            mode: 'markers',
-            type: 'scatter',
-            showlegend: true,
-        }];
-        const layout = {
-            xaxis: {
-                title: {
-                    text: 'Events Processed'
-                },
-                rangemode: 'tozero',
-                color: 'white',
-            },
-            yaxis: {
-                title: {
-                    text: 'Events Rolled Back'
-                },
-                rangemode: 'tozero',
-                color: 'white',
-            },
-            paper_bgcolor: '1d232a',
-            plot_bgcolor: '1d232a',
-            margin: {
-                l: 50,
-                r: 50,
-                b: 50,
-                t: 50,
-                pad: 4
-            }
-        };
-        const config = { responsive: true };
-        Plotly.newPlot(this.scatterPlotEl, data, layout, config);
-
+      const data = [
+        {
+          x: [],
+          y: [],
+          mode: 'markers',
+          type: 'scatter',
+          showlegend: true,
+        },
+      ];
+      const layout = {
+        xaxis: {
+          title: {
+            text: 'Events Processed',
+          },
+          rangemode: 'tozero',
+          color: 'white',
+        },
+        yaxis: {
+          title: {
+            text: 'Events Rolled Back',
+          },
+          rangemode: 'tozero',
+          color: 'white',
+        },
+        paper_bgcolor: '1d232a',
+        plot_bgcolor: '1d232a',
+        margin: {
+          l: 50,
+          r: 50,
+          b: 50,
+          t: 50,
+          pad: 4,
+        },
+      };
+      const config = { responsive: true };
+      Plotly.newPlot(this.scatterPlotEl, data, layout, config);
     },
 
     async load() {
-        this.initPlot();
-        await this.loadRossData();
-        this.isLoaded = true;
+      this.initPlot();
+      await this.loadRossData();
+      this.isLoaded = true;
     },
 
     async loadRossData() {
-        this.isLoaded = false;
-        const payload = await this.$store.dataStore.fetchRossData();
-        this.columns = payload.columns ?? [];
-        this.records = payload.data ?? [];
-        this.updatePlotData();
+      this.isLoaded = false;
+      const payload = await this.$store.dataStore.fetchRossData();
+      this.columns = payload.columns ?? [];
+      this.records = payload.data ?? [];
+      this.updatePlotData();
     },
-
 
     /**
      * Update the plot with current axis selections.
      */
     updatePlotData() {
-        if (!this.scatterPlotEl || this.records.length === 0) {
-            return;
-        }
+      if (!this.scatterPlotEl || this.records.length === 0) {
+        return;
+      }
 
-        const xData = this.records.map(record => record[this.selectedXAxis]);
-        const yData = this.records.map(record => record[this.selectedYAxis]);
+      const xData = this.records.map((record) => record[this.selectedXAxis]);
+      const yData = this.records.map((record) => record[this.selectedYAxis]);
 
-        // TODO: This could be another choice we allow the user to make.
-        const color_range = this.records.map(record => record.PE_ID);
+      // TODO: This could be another choice we allow the user to make.
+      const colorRange = this.records.map((record) => record.PE_ID);
 
-        Plotly.update(this.scatterPlotEl, {
-            x: [xData],
-            y: [yData],
-            marker: {
-                color: color_range,
-                colorscale: 'Blues'
-            }
-        }, {
-            xaxis: {
-                title: {
-                    text: this.valueList.find((item) => item.key === this.selectedXAxis).label,
-                },
-                color: 'white',
+      Plotly.update(
+        this.scatterPlotEl,
+        {
+          x: [xData],
+          y: [yData],
+          marker: {
+            color: colorRange,
+            colorscale: 'Blues',
+          },
+        },
+        {
+          xaxis: {
+            title: {
+              text: this.valueList.find((item) => item.key === this.selectedXAxis).label,
             },
-            yaxis: {
-                title: {
-                    text: this.valueList.find((item) => item.key === this.selectedYAxis).label,
-                },
-                color: 'white',
-            }
-        });
-    }
-    }));
+            color: 'white',
+          },
+          yaxis: {
+            title: {
+              text: this.valueList.find((item) => item.key === this.selectedYAxis).label,
+            },
+            color: 'white',
+          },
+        },
+      );
+    },
+  }));
 });
--- net_maestro/core/static/net_maestro/plots/timePlot.js
+++ net_maestro/core/static/net_maestro/plots/timePlot.js
@@ -4,159 +4,161 @@
  */
 
 document.addEventListener('alpine:init', () => {
-    Alpine.data('timePlot', () => ({
-        records: [],
-        columns: [],
-        selectedXAxis: 'virtual_time',
-        selectedYAxis: 'events_processed',
-        minTime: null,
-        maxTime: null,
-        timePlotEl: null,
-        isPlotInitialized: false,
-        isLoaded: false,
+  Alpine.data('timePlot', () => ({
+    records: [],
+    columns: [],
+    selectedXAxis: 'virtual_time',
+    selectedYAxis: 'events_processed',
+    minTime: null,
+    maxTime: null,
+    timePlotEl: null,
+    isPlotInitialized: false,
+    isLoaded: false,
 
-        get xAxisValues() {
-            return [
-                { key: "virtual_time", label: "Virtual Time"},
-                { key: "real_time", label: "Real Time"},
-            ]
-        },
-        get yAxisValues() {
-            const excluded_columns = ["PE_ID", "real_time", "virtual_time"]
-            const filtered_columns = this.columns.filter(column => column && !excluded_columns.includes(column))
+    get xAxisValues() {
+      return [
+        { key: 'virtual_time', label: 'Virtual Time' },
+        { key: 'real_time', label: 'Real Time' },
+      ];
+    },
+    get yAxisValues() {
+      const excludedColumns = ['PE_ID', 'real_time', 'virtual_time'];
+      const filteredColumns = this.columns.filter(
+        (column) => column && !excludedColumns.includes(column),
+      );
 
-            return filtered_columns.map((value) => ({
-                key: value,
-                label: value.replaceAll("_", " ")
-            }))
-        },
-
+      return filteredColumns.map((value) => ({
+        key: value,
+        label: value.replaceAll('_', ' '),
+      }));
+    },
 
     /**
      * Initialize the component and set up watchers.
      * Called automatically by Alpine.js when component mounts.
      */
     init() {
-        this.$watch('$store.dataStore.loadTick', () => {
-            this.load();
-        });
+      this.$watch('$store.dataStore.loadTick', () => {
+        this.load();
+      });
     },
 
     /**
      * Initialize the Plotly time plot.
      */
     initPlot() {
-        if (this.isPlotInitialized) {
-            return;
-        }
-        this.isPlotInitialized = true;
-        this.timePlotEl = document.getElementById('timePlot');
-        if (!this.timePlotEl) {
-            return;
-        }
+      if (this.isPlotInitialized) {
+        return;
+      }
+      this.isPlotInitialized = true;
+      this.timePlotEl = document.getElementById('timePlot');
+      if (!this.timePlotEl) {
+        return;
+      }
 
-        const data = [{
-            x: [],
-            y: [],
-            showlegend: true,
-        }];
-        const layout = {
-            xaxis: {
-                title: {
-                    text: 'Virtual Time'
-                },
-                rangemode: 'tozero',
-                color: 'white',
-            },
-            yaxis: {
-                title: {
-                    text: 'Events Processed'
-                },
-                rangemode: 'tozero',
-                color: 'white',
-            },
-            paper_bgcolor: '1d232a',
-            plot_bgcolor: '1d232a',
-            margin: {
-                l: 50,
-                r: 50,
-                b: 50,
-                t: 50,
-                pad: 4
-            }
-        };
-        const config = { responsive: true };
-        Plotly.newPlot(this.timePlotEl, data, layout, config);
+      const data = [
+        {
+          x: [],
+          y: [],
+          showlegend: true,
+        },
+      ];
+      const layout = {
+        xaxis: {
+          title: {
+            text: 'Virtual Time',
+          },
+          rangemode: 'tozero',
+          color: 'white',
+        },
+        yaxis: {
+          title: {
+            text: 'Events Processed',
+          },
+          rangemode: 'tozero',
+          color: 'white',
+        },
+        paper_bgcolor: '1d232a',
+        plot_bgcolor: '1d232a',
+        margin: {
+          l: 50,
+          r: 50,
+          b: 50,
+          t: 50,
+          pad: 4,
+        },
+      };
+      const config = { responsive: true };
+      Plotly.newPlot(this.timePlotEl, data, layout, config);
     },
 
     async load() {
-        this.initPlot();
-        await this.loadRossData();
-        this.isLoaded = true;
+      this.initPlot();
+      await this.loadRossData();
+      this.isLoaded = true;
     },
 
     async loadRossData() {
-        this.isLoaded = false;
-        const payload = await this.$store.dataStore.fetchRossData();
-        this.columns = payload.columns ?? [];
-        this.records = payload.data ?? [];
-        this.updatePlotData();
+      this.isLoaded = false;
+      const payload = await this.$store.dataStore.fetchRossData();
+      this.columns = payload.columns ?? [];
+      this.records = payload.data ?? [];
+      this.updatePlotData();
     },
-
 
     /**
      * Update the plot with current axis selections.
      * Groups data by PE_ID to create separate traces.
      */
     updatePlotData() {
-        if (!this.timePlotEl || this.records.length === 0) {
-            return;
+      if (!this.timePlotEl || this.records.length === 0) {
+        return;
+      }
+
+      const groupedData = {};
+
+      this.records.forEach((record) => {
+        const peId = record.PE_ID;
+        if (!groupedData[peId]) {
+          groupedData[peId] = {
+            x: [],
+            y: [],
+            peId: peId,
+          };
         }
+        groupedData[peId].x.push(record[this.selectedXAxis]);
+        groupedData[peId].y.push(record[this.selectedYAxis]);
+      });
 
-        const groupedData = {};
+      const traces = Object.values(groupedData).map((peData) => ({
+        x: peData.x,
+        y: peData.y,
+        showlegend: true,
+      }));
 
-        this.records.forEach(record => {
-            const peId = record.PE_ID;
-            if (!groupedData[peId]) {
-                groupedData[peId] = {
-                    x: [],
-                    y: [],
-                    peId: peId
-                };
-            }
-            groupedData[peId].x.push(record[this.selectedXAxis]);
-            groupedData[peId].y.push(record[this.selectedYAxis]);
-        });
-
-        const traces = Object.values(groupedData).map(peData => ({
-            x: peData.x,
-            y: peData.y,
-            showlegend: true
-        }));
-
-        Plotly.react(this.timePlotEl, traces, {
-            xaxis: {
-                title: {
-                    text: this.xAxisValues.find((item) => item.key === this.selectedXAxis).label,
-                },
-                color: 'white',
-            },
-            yaxis: {
-                title: {
-                    text: this.yAxisValues.find((item) => item.key === this.selectedYAxis).label,
-                },
-                color: 'white',
-            },
-            paper_bgcolor: '1d232a',
-            plot_bgcolor: '1d232a',
-            margin: {
-                l: 50,
-                r: 50,
-                b: 50,
-                t: 50,
-                pad: 4
-            }
-        });
-    }
-    }));
+      Plotly.react(this.timePlotEl, traces, {
+        xaxis: {
+          title: {
+            text: this.xAxisValues.find((item) => item.key === this.selectedXAxis).label,
+          },
+          color: 'white',
+        },
+        yaxis: {
+          title: {
+            text: this.yAxisValues.find((item) => item.key === this.selectedYAxis).label,
+          },
+          color: 'white',
+        },
+        paper_bgcolor: '1d232a',
+        plot_bgcolor: '1d232a',
+        margin: {
+          l: 50,
+          r: 50,
+          b: 50,
+          t: 50,
+          pad: 4,
+        },
+      });
+    },
+  }));
 });
