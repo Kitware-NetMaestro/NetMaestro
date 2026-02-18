4a4672d5ecfb42df93ba9d8aa84f24475cbe282b
Brianna Major <brianna.major@kitware.com>
2026-02-18 10:33:14 -0500

    Update event parser and add celery task integration
    - Refactored EventFile parser to use generator pattern
        - yields individual records as dicts; memory-efficient streaming
    - Updated read() method to consume the generator
    - Modified EventFile parser to accept either filename or bytes
    - Integrated data_ingest management command with Celery task

--- net_maestro/core/tasks/events.py
+++ net_maestro/core/tasks/events.py
@@ -5,7 +5,7 @@ from django.db import transaction
 
 from net_maestro.core.models.event_file import EventFile
 from net_maestro.core.models.event_record import EventRecord
-from net_maestro.core.parsers.event_trace_file import EventFile as EventFileParser
+from net_maestro.core.parsers.event_trace_file import EventFileParser
 
 logger = logging.getLogger(__name__)
 
@@ -20,17 +20,5 @@ def run_event_task(event_file_pk: int) -> dict:
 
     with transaction.atomic():
         parser = EventFileParser(content)
-        batch = []
-        for record_dict in parser.parse_event_records():
-            # Create EventRecord instance (not saved yet)
-            event_record = EventRecord(
-                event_file=event_file_model,
-                source_lp=str(record_dict['source_lp']),
-                dest_lp=str(record_dict['dest_lp']),
-                time_step=record_dict['time_step'],
-                virtual_send=str(record_dict['virtual_send']),
-                virtual_receive=str(record_dict['virtual_receive']),
-                event_type=str(record_dict['event_type']),
-            )
-            batch.append(event_record)
+        batch = [EventRecord(**rec_dict) for rec_dict in parser.parse_event_records()]
         EventRecord.objects.bulk_create(batch)
