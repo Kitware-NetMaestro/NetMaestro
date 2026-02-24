c6fefd1f350b3c1a3fab32d626d1733657bacee0
Mary Salvi <mary.salvi@kitware.com>
2026-02-24 17:38:03 -0500

    Add simulation_file to admin page

--- net_maestro/core/admin/__init__.py
+++ net_maestro/core/admin/__init__.py
@@ -3,9 +3,11 @@ from __future__ import annotations
 from .event_file import EventFileAdmin
 from .event_record import EventRecordAdmin
 from .run import RunAdmin
+from .simulation_file import SimulationFileAdmin
 
 __all__ = [
     "EventFileAdmin",
     "EventRecordAdmin",
     "RunAdmin",
+    "SimulationFileAdmin",
 ]
