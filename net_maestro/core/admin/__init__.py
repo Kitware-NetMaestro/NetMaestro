7104b4ac831a3dfd608ec922e9fcfb990f24a0bf
Mary Salvi <mary.salvi@kitware.com>
2026-03-05 16:11:03 -0500

    Add sim KP and LP record models

--- net_maestro/core/admin/__init__.py
+++ net_maestro/core/admin/__init__.py
@@ -4,6 +4,8 @@ from .event_file import EventFileAdmin
 from .event_record import EventRecordAdmin
 from .run import RunAdmin
 from .simulation_file import SimulationFileAdmin
+from .simulation_kp_record import SimulationKpRecordAdmin
+from .simulation_lp_record import SimulationLpRecordAdmin
 from .simulation_pe_record import SimulationPeRecordAdmin
 
 __all__ = [
@@ -11,5 +13,7 @@ __all__ = [
     "EventRecordAdmin",
     "RunAdmin",
     "SimulationFileAdmin",
+    "SimulationKpRecordAdmin",
+    "SimulationLpRecordAdmin",
     "SimulationPeRecordAdmin",
 ]
