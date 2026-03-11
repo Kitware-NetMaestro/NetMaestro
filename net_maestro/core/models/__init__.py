94707004141dd8fd326135687a443b1979f0ccb6
Brianna Major <brianna.major@kitware.com>
2026-03-11 17:34:09 -0400

    Add the model file model

--- net_maestro/core/models/__init__.py
+++ net_maestro/core/models/__init__.py
@@ -2,6 +2,7 @@ from __future__ import annotations
 
 from .event_file import EventFile
 from .event_record import EventRecord
+from .model_file import ModelFile
 from .run import Run
 from .simulation_base_record import SimulationBaseRecord
 from .simulation_file import SimulationFile
@@ -12,6 +13,7 @@ from .simulation_pe_record import SimulationPeRecord
 __all__ = [
     "EventFile",
     "EventRecord",
+    "ModelFile",
     "Run",
     "SimulationBaseRecord",
     "SimulationFile",
---
+++ net_maestro/core/models/model_file.py
@@ -0,0 +1,16 @@
+from __future__ import annotations
+
+from django.db import models
+from s3_file_field import S3FileField
+
+from net_maestro.core.models.run import Run
+
+
+class ModelFile(models.Model):
+    run = models.ForeignKey(Run, on_delete=models.CASCADE, related_name="model_files")
+    uploaded = models.DateTimeField(auto_now_add=True)
+
+    file = S3FileField()
+
+    def __str__(self) -> str:
+        return f"ModelFile {self.id}"
