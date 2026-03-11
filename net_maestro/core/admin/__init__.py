be517520bb65822315b252cdb6bba1df12d8488c
Brianna Major <brianna.major@kitware.com>
2026-03-11 19:03:25 -0400

    Add the admin pages for model file and model record

--- net_maestro/core/admin/__init__.py
+++ net_maestro/core/admin/__init__.py
@@ -2,6 +2,8 @@ from __future__ import annotations
 
 from .event_file import EventFileAdmin
 from .event_record import EventRecordAdmin
+from .model_file import ModelFileAdmin
+from .model_record import ModelRecordAdmin
 from .run import RunAdmin
 from .simulation_file import SimulationFileAdmin
 from .simulation_kp_record import SimulationKpRecordAdmin
@@ -11,6 +13,8 @@ from .simulation_pe_record import SimulationPeRecordAdmin
 __all__ = [
     "EventFileAdmin",
     "EventRecordAdmin",
+    "ModelFileAdmin",
+    "ModelRecordAdmin",
     "RunAdmin",
     "SimulationFileAdmin",
     "SimulationKpRecordAdmin",
