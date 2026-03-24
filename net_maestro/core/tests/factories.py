446a3370986feafdb7b40f6118d42039976dc09b
Brianna Major <brianna.major@kitware.com>
2026-03-24 15:26:50 -0400

    Add "Run" model factory

--- net_maestro/core/tests/factories.py
+++ net_maestro/core/tests/factories.py
@@ -3,6 +3,9 @@ from __future__ import annotations
 from django.contrib.auth.models import User
 import factory.django
 
+from net_maestro.core.constants import RunStatus
+from net_maestro.core.models import Run
+
 
 class UserFactory(factory.django.DjangoModelFactory[User]):
     class Meta:
@@ -12,3 +15,12 @@ class UserFactory(factory.django.DjangoModelFactory[User]):
     email = factory.Faker("safe_email")
     first_name = factory.Faker("first_name")
     last_name = factory.Faker("last_name")
+
+
+class RunFactory(factory.django.DjangoModelFactory):
+    class Meta:
+        model = Run
+
+    name = factory.Sequence(lambda n: f"TestRun-{n}")
+    description = factory.Faker("text", max_nb_chars=200)
+    status = RunStatus.RUNNING
