3f5a3062b99a806470ce783e8b75d7366985c802
Brianna Major <brianna.major@kitware.com>
2026-04-28 10:47:21 -0400

    Add NodeLink model and admin page
    - NodeLink: directed edge between Nodes within a Topology

---
+++ net_maestro/core/admin/node_link.py
@@ -0,0 +1,17 @@
+from __future__ import annotations
+
+from django.contrib import admin
+
+from net_maestro.core.models import NodeLink
+
+
+@admin.register(NodeLink)
+class NodeLinkAdmin(admin.ModelAdmin):
+    list_select_related = ["topology", "source", "target"]
+    list_display = [
+        "id",
+        "topology",
+        "source",
+        "target",
+    ]
+    list_filter = ["topology"]
