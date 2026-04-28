67941a475d771ad984d9431d63823acd2e58f6e7
Brianna Major <brianna.major@kitware.com>
2026-04-28 10:48:11 -0400

    Add Result model and admin page
    - Result: tracks run status (extracted from Run)

--- net_maestro/core/admin/__init__.py
+++ net_maestro/core/admin/__init__.py
@@ -4,20 +4,28 @@ from .event_file import EventFileAdmin
 from .event_record import EventRecordAdmin
 from .model_file import ModelFileAdmin
 from .model_record import ModelRecordAdmin
+from .node import NodeAdmin
+from .node_link import NodeLinkAdmin
+from .result import ResultAdmin
 from .run import RunAdmin
 from .simulation_file import SimulationFileAdmin
 from .simulation_kp_record import SimulationKpRecordAdmin
 from .simulation_lp_record import SimulationLpRecordAdmin
 from .simulation_pe_record import SimulationPeRecordAdmin
+from .topology import TopologyAdmin
 
 __all__ = [
     "EventFileAdmin",
     "EventRecordAdmin",
     "ModelFileAdmin",
     "ModelRecordAdmin",
+    "NodeAdmin",
+    "NodeLinkAdmin",
+    "ResultAdmin",
     "RunAdmin",
     "SimulationFileAdmin",
     "SimulationKpRecordAdmin",
     "SimulationLpRecordAdmin",
     "SimulationPeRecordAdmin",
+    "TopologyAdmin",
 ]
